c13486014ea694ba13ec9047904803a57b8b2942
LaurieBdt <80465351+LaurieBdt@users.noreply.github.com>
2021-03-18 10:28:24 +0100

    Nouveauportfolio

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-var counter = 1;
-setInterval(function(){
-    document.getElementById('radio' + counter).checked = true;
-    counter++;
-    if(counter >= 5) {
-        counter = 1;
-    }
-}, 5000);
-
 var btn = document.querySelector(".btn-menu")
 var menu = document.querySelector(".menu")
 
