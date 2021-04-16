56b1876b1f8ede6ac8f77ada62dcc727f08f3caa
LaurieBdt <80465351+LaurieBdt@users.noreply.github.com>
2021-04-16 15:47:57 +0200

    sites

--- projets/cupcake1/script.js
+++
@@ -1,7 +0,0 @@
-var btn = document.querySelector(".btn-menu")
-var menu = document.querySelector(".menu")
-
-btn.onclick = function () {
-    menu.classList.toggle("open")
-    btn.classList.toggle("open")
-}
